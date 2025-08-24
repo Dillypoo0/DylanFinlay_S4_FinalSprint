c31fc2d0ae79b6c92bf6859cb5ee7cb334d7e210
Dillypoo0 <Dylan.Finlay@keyin.com>
2025-08-24 19:45:41 -0230

    Delete untitled directory

--- untitled/src/main/java/Repo/TreeRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package Repo;
-package com.example.Repo;
-
-import Model.TreeRecord;
-import com.example.bst.model.TreeRecord;
-import org..springframework.data.jap.repository.JpaRepository;
-
-public interface TreeRecordRepository extends JpaRepository <TreeRecord, Long> { }
